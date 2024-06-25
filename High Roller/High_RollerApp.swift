51bbe70da54c284e52d3e304443f8d49b23cadbc
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-06-25 20:10:44 +0200

    Initial Commit

---
+++ High Roller/High_RollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  High_RollerApp.swift
+//  High Roller
+//
+//  Created by Carlos Román Alcaide on 25/6/24.
+//
+
+import SwiftUI
+
+@main
+struct High_RollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
